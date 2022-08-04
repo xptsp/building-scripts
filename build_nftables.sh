73c8364adf9802c3e5c90da73db05583bc468a99
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2022-08-04 07:13:33 -0500

    Fixed environment variables

--- build_nftables.sh
+++ build_nftables.sh
@@ -8,10 +8,10 @@ test -d ${BUILD}/libnftnl11 || source build_libnftnl11.sh
 #################################################################################
 # Compile latest version of nft for armhf if not already done:
 #################################################################################
-if [[ ! -d cd ${BASE}/nftables/ || "$1" =~ (-f|--force)]]; then  
-	cd ${BASE}
+if [[ ! -d cd ${BUILD}/nftables/ || "$1" =~ (-f|--force)]]; then  
+	cd ${BUILD}
 	git clone git://git.netfilter.org/nftables
-	cd ${BASE}/nftables/
+	cd ${BUILD}/nftables/
 	mkdir -p {install,modded}
 	./autogen.sh
 	./configure --host=arm-linux-gnueabihf --prefix=$PWD/install --with-mini-gmp --without-cli
@@ -27,18 +27,18 @@ NEW_NFTABLES_VER=$(git_version nftables ${LIBNFTNL_BUILD})
 
 #================================================================================
 # Modify existing deb package for "libnftnl-dev" with our compiled files:
-cd ${BASE}/nftables/modded
+cd ${BUILD}/nftables/modded
 DIR=nftables_${NEW_NFTABLES_VER}_armhf
 apt download nftables=${OLD_NFTABLES_VER}
 dpkg-deb -R nftables_${OLD_NFTABLES_VER}_armhf.deb ${DIR}
 rm nftables_${OLD_NFTABLES_VER}_armhf.deb
-cd ${BASE}/nftables/modded/${DIR}
-cp ${BASE}/nftables/install/sbin/nft usr/sbin/
+cd ${BUILD}/nftables/modded/${DIR}
+cp ${BUILD}/nftables/install/sbin/nft usr/sbin/
 rm usr/share/doc/nftables/{changelog,copyright}*
 rm usr/share/doc/nftables/examples/*.nft
-cp ${BASE}/nftables/install/share/nftables/*.nft usr/share/doc/nftables/examples/
-cp ${BASE}/nftables/install/share/doc/nftables/examples/*.nft usr/share/doc/nftables/examples/
-cp ${BASE}/nftables/install/share/man/man8/* usr/share/man/man8/
+cp ${BUILD}/nftables/install/share/nftables/*.nft usr/share/doc/nftables/examples/
+cp ${BUILD}/nftables/install/share/doc/nftables/examples/*.nft usr/share/doc/nftables/examples/
+cp ${BUILD}/nftables/install/share/man/man8/* usr/share/man/man8/
 gzip -f usr/share/man/man8/nft.8
 sed -i "s|^Version: .*|Version: ${NEW_NFTABLES_VER}|" DEBIAN/control
 sed -i "s|^Installed-Size: .*|Installed-Size: $(( $(du -s . | awk '{print $1}') - $(du -s DEBIAN | awk '{print $1}') ))|" DEBIAN/control
@@ -50,19 +50,19 @@ dpkg-deb --build --root-owner-group ${DIR}
 
 #================================================================================
 # Modify existing deb package for "libnftables1" with our compiled files:
-cd ${BASE}/nftables/modded
+cd ${BUILD}/nftables/modded
 DIR=libnftables1_${NEW_NFTABLES_VER}_armhf
 apt download libnftables1=${OLD_NFTABLES_VER}
 dpkg-deb -R libnftables1_${OLD_NFTABLES_VER}_armhf.deb ${DIR}
 rm libnftables1_${OLD_NFTABLES_VER}_armhf.deb
-cd ${BASE}/nftables/modded/${DIR}
+cd ${BUILD}/nftables/modded/${DIR}
 rm usr/lib/arm-linux-gnueabihf/*
 rm -rf usr/share/doc/libnftables1/*
-cp ${BASE}/nftables/COPYING usr/share/doc/libnftables1/ 
-cp -a ${BASE}/nftables/install/lib/libnftables.so.1* usr/lib/arm-linux-gnueabihf/
-cp ${BASE}/nftables/install/share/man/man3/libnftables.3 usr/share/man/man3/
+cp ${BUILD}/nftables/COPYING usr/share/doc/libnftables1/ 
+cp -a ${BUILD}/nftables/install/lib/libnftables.so.1* usr/lib/arm-linux-gnueabihf/
+cp ${BUILD}/nftables/install/share/man/man3/libnftables.3 usr/share/man/man3/
 gzip -f usr/share/man/man3/libnftables.3
-cp ${BASE}/nftables/install/share/man/man5/libnftables-json.5 usr/share/man/man5/
+cp ${BUILD}/nftables/install/share/man/man5/libnftables-json.5 usr/share/man/man5/
 gzip -f usr/share/man/man5/libnftables-json.5
 rm DEBIAN/{shlibs,triggers}
 sed -i "s|^Version: .*|Version: ${NEW_NFTABLES_VER}|" DEBIAN/control
@@ -75,17 +75,17 @@ dpkg-deb --build --root-owner-group ${DIR}
 
 #================================================================================
 # Modify existing deb package for "libnftables-dev" with our compiled files:
-cd ${BASE}/nftables/modded
+cd ${BUILD}/nftables/modded
 DIR=libnftables-dev_${NEW_NFTABLES_VER}_armhf
 apt download libnftables-dev=${OLD_NFTABLES_VER}
 dpkg-deb -R libnftables-dev_${OLD_NFTABLES_VER}_armhf.deb ${DIR}
 rm libnftables-dev_${OLD_NFTABLES_VER}_armhf.deb
-cd ${BASE}/nftables/modded/${DIR}
+cd ${BUILD}/nftables/modded/${DIR}
 rm -rf usr/share/doc/libnftables-dev/*
-cp ${BASE}/nftables/COPYING usr/share/doc/libnftables-dev/
-cp ${BASE}/nftables/install/include/nftables/* usr/include/nftables/
-cp -a ${BASE}/nftables/install/lib/*.so usr/lib/arm-linux-gnueabihf/
-cp ${BASE}/nftables/install/lib/pkgconfig/* usr/lib/arm-linux-gnueabihf/pkgconfig/
+cp ${BUILD}/nftables/COPYING usr/share/doc/libnftables-dev/
+cp ${BUILD}/nftables/install/include/nftables/* usr/include/nftables/
+cp -a ${BUILD}/nftables/install/lib/*.so usr/lib/arm-linux-gnueabihf/
+cp ${BUILD}/nftables/install/lib/pkgconfig/* usr/lib/arm-linux-gnueabihf/pkgconfig/
 sed -i "s|^Version: .*|Version: ${NEW_NFTABLES_VER}|" DEBIAN/control
 sed -i "s|^Installed-Size: .*|Installed-Size: $(du -s usr | awk '{print $1}')|" DEBIAN/control
 sed -i "s|$(grep -o "libnftables1 (= [^)]*)" DEBIAN/control)|libnftables1 (= ${NEW_NFTABLES_VER})|" DEBIAN/control
@@ -93,4 +93,4 @@ find usr -type f -exec md5sum {} \; > DEBIAN/md5sums
 cd ..
 dpkg-deb --build --root-owner-group ${DIR}
 apt install -y ./*.deb
-mv *.deb ${BASE}
+mv *.deb ${BUILD}
